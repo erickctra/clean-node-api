860bb5c87adb8f0b9105c3be2e7673788457f8c9
Erick Cintra <developer.erickcintra@outlook.com>
2022-07-05 16:55:43 -0300

    chore: add jest to lint staged

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
-export default {
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
